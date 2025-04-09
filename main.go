84672d8409abd3a9bfb18ab3ac087125d82a0eaf
omerbenda <omerbenda@gmail.com>
2025-04-09 22:12:10 +0300

    feat(main.go): Add log file

--- main.go
+++ main.go
@@ -1,13 +1,32 @@
 package main
 
 import (
+	"io"
+	"log"
 	"net/http"
+	"os"
 
 	"github.com/gin-gonic/gin"
 	"github.com/omerbenda/redirector/db"
 )
 
 func main() {
+	logFile, err := os.OpenFile("log.txt", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0644)
+
+	if err != nil {
+		log.Fatal("Failed to open log file: ", err)
+	}
+
+	defer logFile.Close()
+
+	multiwriter := io.MultiWriter(logFile, os.Stdout)
+	gin.DefaultWriter = multiwriter
+	gin.DefaultErrorWriter = multiwriter
+	log.SetOutput(gin.DefaultWriter)
+
+	log.Println("Starting server...")
+
+	log.Println("Reading DB")
 	db.Read()
 
 	r := gin.Default()
